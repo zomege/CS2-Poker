be4ab1d36f9bc828f58e1f25ffc85f145c27e0cc
zomege <freneau.ellory@gmail.com>
2015-05-25 18:41:00 -0700

    wrote findWinner
    findWinner() works as described

--- Controller.java
+++ Controller.java
@@ -35,6 +35,10 @@ public class Controller
 
    public static String findWinner(){
       int result = hand1.compareTo(hand2);
-      return "TODO: Who wins?";
+      if(result<0)
+		return "Player 2 won!";
+      if(result==0)
+		return "Niether player won.";
+	return "Player 1 won!";
    }
 }
