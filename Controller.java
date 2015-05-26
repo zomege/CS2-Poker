73259513e4ff50c2970b2be0153e6cddf01c737b
zomege <freneau.ellory@gmail.com>
2015-05-25 18:37:23 -0700

    added comments (also uncommented print lines)
    It's not a true github environment without needless commits!

--- Controller.java
+++ Controller.java
@@ -3,31 +3,31 @@ import java.util.*;
 public class Controller
 {
    private static Deck deck;
-   private static Hand hand1;
+   private static Hand hand1;//players hands
    private static Hand hand2;
 
    public static void main(String[] args){
-      deck = new Deck();
-      //System.out.println(deck);
+      deck = new Deck();//creates deck and hand data types
+      System.out.println(deck);
       hand1 = new Hand();
       hand2 = new Hand();
 
-      dealHands(5);
-      //System.out.println(hand1);
-      //System.out.println(hand2);
+      dealHands(5);//fills both hands with 5 cards
+      System.out.println(hand1);
+      System.out.println(hand2);
 
-      hand1.sortHand();
+      hand1.sortHand();//orders hand
       hand2.sortHand();
       System.out.println(hand1);
       System.out.println(hand2);
 
-      System.out.println("Best Hand: " + hand1.handValue());
+      System.out.println("Best Hand: " + hand1.handValue());//compares
       System.out.println("Best Hand: " + hand2.handValue());
       System.out.println(findWinner());
    }
 
    public static void dealHands(int x){
-      	for(int i=0;i<x;i++){
+      	for(int i=0;i<x;i++){//adds x cards to both hands
 			hand1.add(deck.remove());
 			hand2.add(deck.remove());
 		}
