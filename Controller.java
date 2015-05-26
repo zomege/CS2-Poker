a8999d11def22479f6d91bf753fc104788333dcb
zomege <freneau.ellory@gmail.com>
2015-05-25 18:32:15 -0700

    dealHands functionality
    Wrote the dealHands method and recompiled.

--- Controller.java
+++ Controller.java
@@ -27,7 +27,10 @@ public class Controller
    }
 
    public static void dealHands(int x){
-      //TODO: Deal x cards to each player
+      	for(int i=0;i<x;i++){
+			hand1.add(deck.remove());
+			hand2.add(deck.remove());
+		}
    }
 
    public static String findWinner(){
